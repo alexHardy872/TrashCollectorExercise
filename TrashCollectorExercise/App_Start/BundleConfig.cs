5b1308225ed5aba6b480cf86921e6df1e3d5815f
alexHardy872 <alexhardy872@gmail.com>
2019-10-30 15:40:45 -0500

    updated bootstrap theme

--- TrashCollectorExercise/App_Start/BundleConfig.cs
+++ TrashCollectorExercise/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace TrashCollectorExercise
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap2.css",
                       "~/Content/site.css"));
         }
     }
