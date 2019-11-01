7bc6d493a6229fe404fc2488b04049629dc8f2ea
alexHardy872 <alexhardy872@gmail.com>
2019-11-01 14:14:07 -0500

    inttigrated JS test for API into VS view, initial tests

---
+++ TrashCollectorExercise/Models/ViewModels/GeocoderViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Xml.Linq;
+
+namespace TrashCollectorExercise.Models.ViewModels
+{
+    public class GeocoderViewModel
+    {
+        public Customer customer { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+
+
+        public GeocoderViewModel(Customer customerIn)
+        {
+            customer = customerIn;
+
+
+
+        }
+
+
+    }
+}
